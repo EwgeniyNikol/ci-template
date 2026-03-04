eebe0cb286d8cc4af21c8356fd165920fda6adaf
ewgeny_nikol <ewg240@yandex.ru>
2026-03-04 17:08:53 +0300

    Добавил health.js и тесты

---
+++ src/js/__tests__/health.test.js
@@ -0,0 +1,16 @@
+import getHealthStatus from '../health';
+
+test('should return healthy for health > 50', () => {
+  expect(getHealthStatus({ name: 'Маг', health: 90 })).toBe('healthy');
+});
+
+test('should return wounded for health between 15 and 50', () => {
+  expect(getHealthStatus({ name: 'Маг', health: 50 })).toBe('wounded');
+  expect(getHealthStatus({ name: 'Маг', health: 30 })).toBe('wounded');
+  expect(getHealthStatus({ name: 'Маг', health: 15 })).toBe('wounded');
+});
+
+test('should return critical for health < 15', () => {
+  expect(getHealthStatus({ name: 'Маг', health: 14 })).toBe('critical');
+  expect(getHealthStatus({ name: 'Маг', health: 0 })).toBe('critical');
+});
